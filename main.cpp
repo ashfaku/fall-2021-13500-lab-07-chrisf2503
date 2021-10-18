fe1427cad8838d18afa8d084dfef87ceca856762
chrisf2503 <christopher.flores62@myhunter.cuny.edu>
2021-10-18 16:15:31 -0400

    edited

--- main.cpp
+++ main.cpp
@@ -6,12 +6,20 @@
 int main()
 {
 	int numberOfCerly = 0;
+	std::string line;
+	std::ifstream gin("poorFormat.txt");
+	std::cout << "This is the bad format: \n";
+	while(getline(gin, line)){
+		std::cout << line << '\n';
+	}
+	std::cout << "--------------------------------\n";
+	gin.close();
 	std::ifstream fin("poorFormat.txt");
 	if(fin.fail()){
 		std::cerr << "Fail to open";
 		exit(1);
 	}
-	std::string line;
+	std::cout << "This is the good format: \n";
 	while(getline(fin, line)){
 		line = removeLeadingSpaces(line);
 		std::string tab = "";
@@ -36,6 +44,7 @@ int main()
 			std::cout << line << "\n";
 		}
 	}
+	fin.close();
 	//std::string edit = removeLeadingSpaces("");
 	//int index = foundOpenCerly("if(number == 2)");
 	//std::cout << index  << '\n';
